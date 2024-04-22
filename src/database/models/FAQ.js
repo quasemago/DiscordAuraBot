3b50ae0b6519811da2e4f58cea1a27df18f384b7
quasemago <brunoronningfn@gmail.com>
2024-04-22 17:40:16 -0400

    feat: Criado os modelos de banco de dados referente ao horário de aulas, FAQ, e bolsas.

---
+++ src/database/models/FAQ.js
@@ -0,0 +1,26 @@
+import Sequelize, {Model} from "sequelize";
+
+export default class FAQ extends Model {
+    static init(sequelize) {
+        super.init(
+            {
+                id: {
+                    type: Sequelize.INTEGER,
+                    autoIncrement: true,
+                    primaryKey: true,
+                },
+                question: {
+                    type: Sequelize.STRING,
+                    allowNull: false,
+                },
+                answer: {
+                    type: Sequelize.STRING,
+                    allowNull: false,
+                },
+            }, {
+                sequelize,
+                tableName: "faq",
+            });
+        return this;
+    }
+}
