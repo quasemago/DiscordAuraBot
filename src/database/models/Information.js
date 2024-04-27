cff771bd1548b71b46f6560bfc6ed4df17e28675
quasemago <brunoronningfn@gmail.com>
2024-04-27 18:14:50 -0400

    refactor: Corrige um 'typo' no modelo 'Information'.

--- src/database/models/Information.js
+++ src/database/models/Information.js
@@ -24,7 +24,7 @@ export default class Information extends Model {
                 }
             }, {
                 sequelize,
-                modelName: "information",
+                tableName: "information",
             }
         );
     }
