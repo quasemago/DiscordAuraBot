92914c65ab44920160958641ff0221de4e5eaa03
quasemago <brunoronningfn@gmail.com>
2024-05-03 21:02:45 -0400

    feat: Criado os seeders com sequelize para popular o banco de dados.

---
+++ src/database/seeders/20240502235045-insert-data-classperiod.cjs
@@ -0,0 +1,17 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        const filePath = path.join(__dirname, './sql/dump20240503_class_period.sql');
+        const sql = fs.readFileSync(filePath, {encoding: 'utf-8'});
+        await queryInterface.sequelize.query(sql);
+    },
+
+    async down(queryInterface, Sequelize) {
+        // TODO
+    }
+};
